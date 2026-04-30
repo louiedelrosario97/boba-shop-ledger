fb088a8a18aa4d0911143d09c3b3ec337f001f4b
Louie Delrosario <louie.delrosario97@gmail.com>
2026-04-30 13:03:07 -0700

    Fix package structure to src/com/pluralsight

---
+++ src/com/pluralsight/Main.java
@@ -0,0 +1,189 @@
+package com.pluralsight;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+
+public class Main
+{
+   static Scanner scanner = new Scanner(System.in);
+   static LedgerManager ledger = new LedgerManager();
+
+   static void main(String[] args)
+    {
+        homeScreen();
+    }
+
+    // ---------------------------------------- homeScreen() ----------------------------------------------------------
+   static void homeScreen()
+   {   String choice;
+       boolean runningLoop = true; // Variable used to control the loop via "Close App"
+       while(runningLoop)
+       {
+           System.out.println();
+           System.out.println(" LEDGER HOMESCREEN ");
+           System.out.println("-------------------");
+           System.out.println("D) Add Deposit");
+           System.out.println("P) Add Payment");
+           System.out.println("L) Open Ledger");
+           System.out.println("X) Close App");
+           System.out.print("Choose option: ");
+           choice = scanner.nextLine().toUpperCase().strip();
+
+           switch (choice)
+           {
+               case "D": deposit();           break;
+               case "P": payment();           break;
+               case "L": ledgerScreen();      break;
+               case "X": runningLoop = false; break;
+               default:
+                   System.out.println();
+                   System.out.println("Invalid option. Please try again.");
+           }
+       }
+       System.out.println("Sayonara!");
+   }
+    // ---------------------------------------- ledgerScreen() --------------------------------------------------------
+    public static void ledgerScreen()
+    {
+        String choice;
+        boolean runningLoop = true;
+        while (runningLoop)
+        {
+            System.out.println();
+            System.out.println("---- LEDGER ----");
+            System.out.println("A) Display All");
+            System.out.println("D) Display Deposit History");
+            System.out.println("P) Display Payment History");
+            System.out.println("R) Report Search");
+            System.out.println("H) (Back) to Home Screen");
+            System.out.println("X) Close App");
+            System.out.println("Choose option: ");
+            choice = scanner.nextLine().toUpperCase().strip();
+
+            switch (choice)
+            {
+                case "A": ledger.displayAll();      break;
+                case "D": ledger.displayDeposits(); break;
+                case "P": ledger.displayPayments(); break;
+                case "R": reportsScreen();          break;
+                case "H": homeScreen();             break;
+                case "X": runningLoop = false;      break;
+
+                default:
+                    System.out.println();
+                    System.out.println("Invalid option. Please try again.");
+            }
+        }
+        System.out.println();
+        System.out.println("Sayonara!");
+    }
+    // --------------------------------------------- reportsScreen() --------------------------------------------------
+    public static void reportsScreen()
+    {
+        String choice;
+        boolean runningLoop = true;
+        while(runningLoop)
+        {
+            System.out.println("---- REPORTS ----");
+            System.out.println("1) Month To Date");
+            System.out.println("2) Previous Month");
+            System.out.println("3) Year To Date");
+            System.out.println("4) Previous Year");
+            System.out.println("5) Search by Vendor");
+            System.out.println("0) (Back) to Ledger");
+            System.out.println("H) (Back) to Home Screen");
+            System.out.println("X) Close App");
+            System.out.println("Select report type: ");
+            choice = scanner.nextLine().toUpperCase().strip();
+
+            switch(choice)
+            {
+                case "1": ledger.monthToDate();   break;
+                case "2": ledger.previousMonth(); break;
+                case "3": ledger.yearToDate();    break;
+                case "4": ledger.previousYear();  break;
+                case "5":
+                    System.out.print("Vendor Name: ");
+                    String vendorName = scanner.nextLine();
+                    ledger.searchByVendor(vendorName);
+                                                  break;
+                case "0": ledgerScreen();         break;
+                case "H": homeScreen();           break;
+                case "X": runningLoop = false;    break;
+
+                default:
+                    System.out.println();
+                    System.out.println("Invalid option. Please try again.");
+            }
+        }
+        System.out.println();
+        System.out.println("Sayonara!");
+    }
+    // ---------------------------------------- deposit() -------------------------------------------------------------
+    public static void deposit()
+    {
+        System.out.println();
+        System.out.println("DEPOSIT INFO:");
+        System.out.println("-------------------");
+        System.out.print("Description: ");
+        String description = scanner.nextLine();
+
+        System.out.print("Vendor: ");
+        String vendor = scanner.nextLine();
+
+        System.out.print("Deposit Amount: ");
+        double amount = Double.parseDouble(scanner.nextLine());
+
+        while (amount < 0)
+        {
+            System.out.println();
+            System.out.println("Deposit must be a positive value, please try again.");
+            System.out.print("Amount: " );
+            amount = Double.parseDouble(scanner.nextLine());
+        }
+
+        String date = LocalDate.now().format(DateTimeFormatter.ofPattern("MMMM d, yyyy"));
+        String time = LocalTime.now().format(DateTimeFormatter.ofPattern("hh:mm:ss a")); // "hh:mm:ss a" creates 12-hour AM/PM format
+
+        Transaction newTransaction = new Transaction(date, time, description, vendor, amount);
+        ledger.addTransaction(newTransaction);
+
+        System.out.println();
+        System.out.println("Deposit added successfully! Returning to Homescreen...");
+    }
+    // ---------------------------------------- payment() -------------------------------------------------------------
+    public static void payment()
+    {
+        System.out.println();
+        System.out.println("PAYMENT INFO:");
+        System.out.println("-------------------");
+        System.out.print("Description: ");
+        String description = scanner.nextLine();
+
+        System.out.print("Vendor: ");
+        String vendor = scanner.nextLine();
+
+        System.out.print("Amount: ");
+        double amount = Double.parseDouble(scanner.nextLine());
+
+        while (amount > 0)
+        {
+            System.out.println();
+            System.out.println("Payment must be a negative value, please try again.");
+            System.out.print("Amount: ");
+            amount = Double.parseDouble(scanner.nextLine());
+        }
+
+        String date = LocalDate.now().format(DateTimeFormatter.ofPattern("MMMM d, yyyy"));
+        String time = LocalTime.now().format(DateTimeFormatter.ofPattern("hh:mm:ss a"));
+
+        Transaction newTransaction = new Transaction(date, time, description, vendor, amount);
+        ledger.addTransaction(newTransaction);
+
+        System.out.println();
+        System.out.println("Payment added successfully! Returning to Homescreen...");
+    }
+}
---
+++ src/com/pluralsight/Transaction.java
@@ -0,0 +1,41 @@
+package com.pluralsight;
+
+public class Transaction {
+
+    // Data Members
+    String date;
+    String time;
+    String description;
+    String vendor;
+    double amount;
+
+    // Getters
+    public double getAmount() { return amount; }
+    public String getVendor() { return vendor; }
+    public String getDescription() { return description; }
+    public String getTime() { return time; }
+    public String getDate() { return date; }
+
+    // Setters
+    public void setAmount(double amount) { this.amount = amount; }
+    public void setVendor(String vendor) { this.vendor = vendor; }
+    public void setDescription(String description) { this.description = description; }
+    public void setTime(String time) { this.time = time; }
+    public void setDate(String date) { this.date = date; }
+
+    // Constructor
+    public Transaction(String date, String time, String description, String vendor, double amount)
+    {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("%-14s | %-11s | %-20s | %-20s | $%.2f", date, time, description, vendor, amount);
+    }
+}
